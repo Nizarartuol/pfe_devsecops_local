1d52de6b35b94e217e8afa443ff769e9c6a43d3e
Nizar Loutra <73958657+Nizarartuol@users.noreply.github.com>
2026-05-27 15:06:42 +0100

    Suppression des variables non utilisées

--- services/pfe_devsecops/src/adservice/src/main/java/hipstershop/AdService.java
+++ services/pfe_devsecops/src/adservice/src/main/java/hipstershop/AdService.java
@@ -17,7 +17,6 @@
 package hipstershop;
 
 import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import hipstershop.Demo.Ad;
 import hipstershop.Demo.AdRequest;
@@ -33,7 +32,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.security.SecureRandom;
-import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -54,18 +52,18 @@ public final class AdService {
     int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "9555"));
     healthMgr = new HealthStatusManager();
 
-    server =
-        ServerBuilder.forPort(port)
-            .addService(new AdServiceImpl())
-            .addService(healthMgr.getHealthService())
-            .build()
-            .start();
+    server = ServerBuilder.forPort(port)
+        .addService(new AdServiceImpl())
+        .addService(healthMgr.getHealthService())
+        .build()
+        .start();
     logger.info("Ad Service started, listening on {}", port);
     Runtime.getRuntime()
         .addShutdownHook(
             new Thread(
                 () -> {
-                  // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                  // Use stderr here since the logger may have been reset by its JVM shutdown
+                  // hook.
                   System.err.println(
                       "*** shutting down gRPC ads server since JVM is shutting down");
                   AdService.this.stop();
@@ -86,8 +84,9 @@ public final class AdService {
     /**
      * Retrieves ads based on context provided in the request {@code AdRequest}.
      *
-     * @param req the request containing context.
-     * @param responseObserver the stream observer which gets notified with the value of {@code
+     * @param req              the request containing context.
+     * @param responseObserver the stream observer which gets notified with the
+     *                         value of {@code
      *     AdResponse}
      */
     @Override
@@ -139,7 +138,10 @@ public final class AdService {
     return service;
   }
 
-  /** Await termination on the main thread since the grpc library uses daemon threads. */
+  /**
+   * Await termination on the main thread since the grpc library uses daemon
+   * threads.
+   */
   private void blockUntilShutdown() throws InterruptedException {
     if (server != null) {
       server.awaitTermination();
@@ -147,41 +149,34 @@ public final class AdService {
   }
 
   private static ImmutableListMultimap<String, Ad> createAdsMap() {
-    Ad hairdryer =
-        Ad.newBuilder()
-            .setRedirectUrl("/product/2ZYFJ3GM2N")
-            .setText("Hairdryer for sale. 50% off.")
-            .build();
-    Ad tankTop =
-        Ad.newBuilder()
-            .setRedirectUrl("/product/66VCHSJNUP")
-            .setText("Tank top for sale. 20% off.")
-            .build();
-    Ad candleHolder =
-        Ad.newBuilder()
-            .setRedirectUrl("/product/0PUK6V6EV0")
-            .setText("Candle holder for sale. 30% off.")
-            .build();
-    Ad bambooGlassJar =
-        Ad.newBuilder()
-            .setRedirectUrl("/product/9SIQT8TOJO")
-            .setText("Bamboo glass jar for sale. 10% off.")
-            .build();
-    Ad watch =
-        Ad.newBuilder()
-            .setRedirectUrl("/product/1YMWWN1N4O")
-            .setText("Watch for sale. Buy one, get second kit for free")
-            .build();
-    Ad mug =
-        Ad.newBuilder()
-            .setRedirectUrl("/product/6E92ZMYYFZ")
-            .setText("Mug for sale. Buy two, get third one for free")
-            .build();
-    Ad loafers =
-        Ad.newBuilder()
-            .setRedirectUrl("/product/L9ECAV7KIM")
-            .setText("Loafers for sale. Buy one, get second one for free")
-            .build();
+    Ad hairdryer = Ad.newBuilder()
+        .setRedirectUrl("/product/2ZYFJ3GM2N")
+        .setText("Hairdryer for sale. 50% off.")
+        .build();
+    Ad tankTop = Ad.newBuilder()
+        .setRedirectUrl("/product/66VCHSJNUP")
+        .setText("Tank top for sale. 20% off.")
+        .build();
+    Ad candleHolder = Ad.newBuilder()
+        .setRedirectUrl("/product/0PUK6V6EV0")
+        .setText("Candle holder for sale. 30% off.")
+        .build();
+    Ad bambooGlassJar = Ad.newBuilder()
+        .setRedirectUrl("/product/9SIQT8TOJO")
+        .setText("Bamboo glass jar for sale. 10% off.")
+        .build();
+    Ad watch = Ad.newBuilder()
+        .setRedirectUrl("/product/1YMWWN1N4O")
+        .setText("Watch for sale. Buy one, get second kit for free")
+        .build();
+    Ad mug = Ad.newBuilder()
+        .setRedirectUrl("/product/6E92ZMYYFZ")
+        .setText("Mug for sale. Buy two, get third one for free")
+        .build();
+    Ad loafers = Ad.newBuilder()
+        .setRedirectUrl("/product/L9ECAV7KIM")
+        .setText("Loafers for sale. Buy one, get second one for free")
+        .build();
     return ImmutableListMultimap.<String, Ad>builder()
         .putAll("clothing", tankTop)
         .putAll("accessories", watch)
@@ -198,12 +193,6 @@ public final class AdService {
       return;
     }
     logger.info("Stats enabled, but temporarily unavailable");
-
-    long sleepTime = 10; /* seconds */
-    int maxAttempts = 5;
-
-    // TODO(arbrown) Implement OpenTelemetry stats
-
   }
 
   private static void initTracing() {
@@ -214,8 +203,6 @@ public final class AdService {
     logger.info("Tracing enabled but temporarily unavailable");
     logger.info("See https://github.com/GoogleCloudPlatform/microservices-demo/issues/422 for more info.");
 
-    // TODO(arbrown) Implement OpenTelemetry tracing
-    
     logger.info("Tracing enabled - Stackdriver exporter initialized.");
   }
 
@@ -223,10 +210,10 @@ public final class AdService {
   public static void main(String[] args) throws IOException, InterruptedException {
 
     new Thread(
-            () -> {
-              initStats();
-              initTracing();
-            })
+        () -> {
+          initStats();
+          initTracing();
+        })
         .start();
 
     // Start the RPC server. You shouldn't see any output from gRPC before this.
